3db6bfded47ac8138d1be897b37eaac182a5fa4d
yohackaboi <anuj.dhillon.14@gmail.com>
2021-04-24 13:50:10 +0530

    changes

--- api/models.py
+++ api/models.py
@@ -3,11 +3,3 @@ from datetime import datetime
 
 # Create your models here.
 
-class Comment(models.Model):
-  content = models.CharField(max_length=200)
-  author = models.CharField(max_length=50,default="anonymous")
-  date_created = models.DateTimeField(default=datetime.now)
-
-      
-  def __str__(self):
-    return self.content
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,3 @@
 from rest_framework import serializers
 from .models import Comment
 
-class CommentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Comment
-		fields ='__all__'
